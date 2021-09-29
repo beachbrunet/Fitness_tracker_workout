07e46291ffcdc662b6a335b0cb77fe0e94bcf243
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-29 11:04:30 -0600

    server just need the routes

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// added from 08 example
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// DB
+const db = require("./models");
+moongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/#", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+require("./routes/#")(app);
+require("./routes/#")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
