943e3907a515783021442243a1c252ca2c791115
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-30 10:56:50 -0600

    re working htmlRoute

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,15 +1,23 @@
 // include a path to get correct file for the html
+const router = require("express").Router();
 const path = require("path");
+// const router = require("./apiRoutes");
 
 // get method to bring user to the exercise page
 // and bring the user to the stats page
+// index html
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-// module.exports = function (app) {
-//   app.get("/exercise"), (req, res) => {
-//     res.sendFile("/exercise.html");
-//   });
-// app.get ("/stats"), (req , res) => {
-//     res.sendFile("/stats.html");
-// }
-// }
-// // path.join(__dirname, "../public");
+// exercise html
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// stats html
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
