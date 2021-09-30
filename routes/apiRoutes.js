8feff91763f76269de8eef8ad8ed2b6a674b593c
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-30 10:08:36 -0600

    fixed syntax

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,6 @@ const router = express.Router();
 
 // call api - getLastWorkout
 
-
 // Find/get workouts data from the backend
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
@@ -31,40 +30,31 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-    // Add exercise api
-router.put('workouts/:id', (req,res) => {
-    const workoutID = req.params.id;
-    console.log(workoutID);
-    console.log(req.body);
-    db.Workout.something (workoutID, { $push: { excercies: req.body}})
-        .then((dbWorkout) = > {
-            res.json(dbWorkout);
-        })
-        .catch((err) => {
-            res.json(err);
-          });
+// Add exercise api
+router.put("workouts/:id", (req, res) => {
+  const workoutID = req.params.id;
+  console.log(workoutID);
+  console.log(req.body);
+  db.Workout.something(workoutID, { $push: { excercies: req.body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
+});
 
-    app.post("/api/workouts",({ body }) => {
-        console.log(`body ${body}`);
+app.post("/api/workouts", ({ body }) => {
+  console.log(`body ${body}`);
 
-        db.Workout.create()
-          .then((dbWorkout) => {
-            res.json(dbWorkout);
-          })
-          .catch((err) => {
-            res.json(err);
-          });
-      });
-
-
-
-
-
-
-
-
-
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 // Example code:
 // app.get("/find/:id", (req, res) => {
