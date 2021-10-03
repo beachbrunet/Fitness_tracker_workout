8b20d626b8107096428c57fb677b1286bed57d2b
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-03 14:01:39 -0600

    spelling errors fixed in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // added base code
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -8,19 +8,20 @@ const PORT = process.env.PORT || 3001;
 
 // added from 08 example -- middleware
 app.use(logger("dev"));
+// read about logger and morgan
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // MongoDB connection
 // const db = require("./models");
-moongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
-
+// link routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
